78d51c6d50d408589ac8542e7f3404520b9590dd
Kren <Hannah.Kren@stlcop.local>
2017-10-31 22:11:30 -0500

    Complete

--- Question3.R
+++ Question3.R
@@ -28,3 +28,10 @@ ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
   scale_x_log10() + scale_y_log10() + geom_point() +
   facet_wrap(~order, nrow = 4, ncol = 5)
 
+#plot five 
+ggplot(data = dat, aes(x = mass.g., y = newborn.g.)) +
+  labs(x = "Adult Mass", y = "Newborn Mass") +
+  scale_x_log10() + scale_y_log10() + geom_point() +
+  geom_smooth(method = "lm")+
+  facet_wrap(~order, nrow = 4,ncol = 5)
+
